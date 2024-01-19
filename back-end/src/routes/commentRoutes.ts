bffe0d781b99d01b1c54619af47db4dd659c5eac
gagkjayawardana <74050058+gagkjayawardana@users.noreply.github.com>
2024-01-19 17:48:06 +0530

    Implement guard routes

--- back-end/src/routes/commentRoutes.ts
+++ back-end/src/routes/commentRoutes.ts
@@ -2,9 +2,10 @@ import { addCommentRules } from '../validation/commentValidation'
 import { addCommentController } from '../controllers/commentController'
 import express from 'express'
 import { checkValidation } from '../validation/checkValidation'
+import { authorization } from '../middleware/authGuards'
 
 const router = express.Router()
 
-router.route('/addComment').post(addCommentRules(), checkValidation, addCommentController)
+router.route('/addComment').post(authorization, addCommentRules(), checkValidation, addCommentController)
 
 export default router
--- back-end/src/routes/postRoutes.ts
+++ back-end/src/routes/postRoutes.ts
@@ -7,12 +7,15 @@ import {
 import express from 'express'
 import { checkValidation } from '../validation/checkValidation'
 import { addFeedbackRules, addNewPostRules, changeStatusRules } from '../validation/postValidation'
+import { adminPermisions, authorization } from '../middleware/authGuards'
 
 const router = express.Router()
 
-router.route('/newPost').post(addNewPostRules(), checkValidation, addNewPostController)
-router.route('/addFeedback/:postId').put(addFeedbackRules(), checkValidation, addFeedbackController)
-router.route('/changeStatus/:postId').put(changeStatusRules(), checkValidation, changePostStatusController)
-router.route('/deletePost/:postId').delete(deletePostController)
+router.route('/newPost').post(authorization, addNewPostRules(), checkValidation, addNewPostController)
+router.route('/addFeedback/:postId').put(adminPermisions, addFeedbackRules(), checkValidation, addFeedbackController)
+router
+  .route('/changeStatus/:postId')
+  .put(adminPermisions, changeStatusRules(), checkValidation, changePostStatusController)
+router.route('/deletePost/:postId').delete(authorization, deletePostController)
 
 export default router
